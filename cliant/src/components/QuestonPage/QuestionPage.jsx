1f1263a5c74b912006a4fc386a484969d2c49bba
Abel Asmelash <abelasmelash07@gamil.com>
2025-10-07 09:14:59 +0200

    first commit, new branch

---
+++ cliant/src/components/QuestonPage/QuestionPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import QuestionForm from "../QuestionForm/QuestionForm";
+import AskAi from "../AskAi/AskAi";
+
+const QuestionPage = () => {
+  return (
+    <>
+      <QuestionForm />
+      <AskAi />
+    </>
+  );
+};
+
+export default QuestionPage;
